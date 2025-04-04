6ab8e34a256227cce627015a67e502cdc956f787
margleb <margleb.dm@gmail.com>
2025-04-04 08:22:22 +0300

    добавлена возможность пренятия событий

--- dialogs/moderate_events/moderate_events.py
+++ dialogs/moderate_events/moderate_events.py
@@ -4,7 +4,7 @@ from aiogram.types import CallbackQuery
 from aiogram_dialog import Dialog, Window, DialogManager
 from aiogram_dialog.widgets.kbd import Button, Row
 from aiogram_dialog.widgets.text import Const, Format, Jinja
-from sqlalchemy import select, func
+from sqlalchemy import select, func, update
 
 from models import Event
 
@@ -20,6 +20,7 @@ async def get_events_data(dialog_manager: DialogManager, **kwargs) -> dict:
     event = await session.scalar(select(Event).where(Event.moderation.is_(False)).limit(1).offset(current_index))
     total_events = await session.scalar(select(func.count(Event.id)).where(Event.moderation.is_(False)))
     dialog_manager.dialog_data['total_events'] = total_events
+    dialog_manager.dialog_data['event_id'] = event.id
 
     return {
         "description": event.description,
@@ -45,6 +46,16 @@ async def on_next_event(callback: CallbackQuery, button: Button, dialog_manager:
 
     data["current_index"] = (current_index + 1) % total_events
 
+
+async def on_accept_event(callback: CallbackQuery, button: Button, dialog_manager: DialogManager):
+
+    session = dialog_manager.middleware_data.get("session")
+    event_id = dialog_manager.dialog_data.get("event_id")
+    stmt = update(Event).where(Event.id == event_id).values({"moderation": True})
+
+    await session.execute(stmt)
+    await session.commit()
+
 dialog_moderate_dialog = Dialog(
     Window(
         Jinja(
@@ -57,6 +68,8 @@ dialog_moderate_dialog = Dialog(
         ),
         Row(
             Button(Const("◀ Назад"), id="prev_event", on_click=on_prev_event, when=F["dialog_data"]["total_events"] > 1),
+            Button(Const("✅ Принять"), id="accept_event", on_click=on_accept_event),
+            Button(Const("❌ Отклонить"), id="reject_event"),
             Button(Const("▶ Вперед"), id="next_event", on_click=on_next_event, when=F["dialog_data"]["total_events"] > 1),
         ),
         parse_mode="HTML",
