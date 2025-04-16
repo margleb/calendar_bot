1aa6226cf4eaa2b317b417d6a9b1070affe7220a
margleb <margleb.dm@gmail.com>
2025-04-16 11:28:12 +0300

    базовая реализация календаря

--- dialogs/widgets/calendar.py
+++ dialogs/widgets/calendar.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from aiogram.types import InlineKeyboardButton
 
 from aiogram_dialog import DialogManager
 from aiogram_dialog.widgets.kbd import (
@@ -8,16 +9,46 @@ from aiogram_dialog.widgets.kbd import (
 )
 from aiogram_dialog.widgets.kbd.calendar_kbd import (
     CalendarScopeView,
-    CalendarDaysView,
     CalendarMonthView,
-    CalendarYearsView
+    CalendarYearsView, CalendarDaysView, raw_from_date, CallbackGenerator
 )
+
+class EventCalendarDaysView(CalendarDaysView):
+    def __init__(self, callback_generator: CallbackGenerator, **kwargs):
+        super().__init__(callback_generator)
+
+    async def _render_date_button(
+            self,
+            selected_date: date,
+            today: date,
+            data: dict,
+            manager: DialogManager,
+    ) -> InlineKeyboardButton:
+        current_data = {
+            "date": selected_date,
+            "data": data,
+        }
+        if selected_date == today:
+            text = self.today_text
+        else:
+            text = self.date_text
+
+        raw_date = raw_from_date(selected_date)
+
+        suffix = "🔥"
+        rendered_text = await text.render_text(current_data, manager)
+
+        return InlineKeyboardButton(
+            # text=f"{suffix}{rendered_text}",
+            text=f"{suffix}",
+            callback_data=self.callback_generator(str(raw_date)),
+        )
 
 class EventCalendar(Calendar):
 
     def _init_views(self) -> dict[CalendarScope, CalendarScopeView]:
         return {
-            CalendarScope.DAYS: CalendarDaysView(self._item_callback_data),
+            CalendarScope.DAYS: EventCalendarDaysView(self._item_callback_data),
             CalendarScope.MONTHS: CalendarMonthView(self._item_callback_data),
             CalendarScope.YEARS: CalendarYearsView(self._item_callback_data),
         }
