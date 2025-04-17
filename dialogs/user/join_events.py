cb147f4d8a15b12dcf4c01db10fe5d604e10afd1
margleb <margleb.dm@gmail.com>
2025-04-17 21:32:33 +0300

    настроил переключение мероприятий

--- dialogs/user/join_events.py
+++ dialogs/user/join_events.py
@@ -4,10 +4,9 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import CallbackQuery
 from aiogram_dialog import Dialog, Window, DialogManager
 from aiogram_dialog.api.entities import MediaAttachment, MediaId
-from aiogram_dialog.widgets.kbd import Cancel, Button
+from aiogram_dialog.widgets.kbd import Cancel, Button, Row
 from aiogram_dialog.widgets.text import Const, Jinja
 from sqlalchemy import select, and_, func
-from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import selectinload
 
 from db.models import User, Association, Event
@@ -17,11 +16,30 @@ from lexicon.lexicon import D_BUTTONS, DU_JOIN_EVENTS
 class DJoinEvents(StatesGroup):
     events = State()
 
+
+async def switch_event(callback_query: CallbackQuery, button: Button, manager: DialogManager):
+
+    total_events = manager.dialog_data.get('total_events')
+    offset = manager.dialog_data.get('offset', 0)
+
+    # Увеличиваем или уменьшаем offset в зависимости от кнопки
+    if button.widget_id == "next_event":
+        offset = (offset + 1) % total_events
+    elif button.widget_id == "prev_event":
+        offset = (offset - 1 + total_events) % total_events
+
+    manager.dialog_data['offset'] = offset
+
+
 async def get_user_events(dialog_manager:DialogManager, **kwargs):
     session = dialog_manager.middleware_data['session']
+    offset = dialog_manager.dialog_data.get('offset')
     user_id = dialog_manager.event.from_user.id
     stmt = (
-        select(Event).
+        select(
+            Event,
+            func.count(Event.id).over().label('total_events') # общее количество
+        ).
         join(Event.user). # association
         join(Association.user). # event
         options(selectinload(Event.user)). # получаем в ассоциативной таблице ВСЕХ пользователей
@@ -32,25 +50,25 @@ async def get_user_events(dialog_manager:DialogManager, **kwargs):
                 Event.date_event >= func.current_date() # текущая или позже дата
             )
         ).
-        # offset(1).
+        offset(offset).
         limit(1)
     )
 
-    # print(stmt.compile(dialect=postgresql.dialect(), compile_kwargs={"literal_binds": True}))
+    result = await session.execute(stmt)
+    row = result.first()
 
-    event = await session.scalar(stmt)
-    if event is None:
-        return {
-            'has_event': False,
-        }
-    else:
-        dialog_manager.dialog_data['event_id'] = event.id
+    if row is None:
+        return {'has_events': False}
 
+    event, total_events = row
+    dialog_manager.dialog_data['event_id'] = event.id
+    dialog_manager.dialog_data['total_events'] = total_events
     total_join = sum(1 for assoc in event.user if assoc.status == 'join')
 
     photo = None
     if event.image_id:
         photo = MediaAttachment(ContentType.PHOTO, file_id=MediaId(event.image_id))
+
 
     return {
         'title': event.title,
@@ -60,7 +78,7 @@ async def get_user_events(dialog_manager:DialogManager, **kwargs):
         'date': event.date_event,
         'city': event.city.value,
         'username': event.username,
-        'has_event': True
+        'has_events': True,
     }
 
 
@@ -87,7 +105,12 @@ async def cancel_event(callback_query: CallbackQuery, button: Button, manager: D
 dialog = Dialog(
     Window(
         Jinja(DU_JOIN_EVENTS['result']),
-        Button(Const(DU_JOIN_EVENTS['buttons']['cancel_event']), id='cancel_event', on_click=cancel_event, when=F['has_event']),
+        Button(Const(DU_JOIN_EVENTS['buttons']['cancel_event']), id='cancel_event', on_click=cancel_event, when=F['has_events']),
+        Row(
+            Button(Const(DU_JOIN_EVENTS['buttons']['prev_event']), id='prev_event', on_click=switch_event),
+           Button(Const(DU_JOIN_EVENTS['buttons']['next_event']), id='next_event', on_click=switch_event),
+           when=F['dialog_data']['total_events'] > 1
+        ),
         Cancel(Const(D_BUTTONS['back'])),
         state=DJoinEvents.events,
         getter=get_user_events,
