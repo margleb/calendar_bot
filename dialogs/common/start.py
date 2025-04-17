59b56b20857e6e3375f9e3c6e9570ba2de5ec3d1
margleb <margleb.dm@gmail.com>
2025-04-17 08:43:01 +0300

    добавлено "написать разработчику"

--- dialogs/common/start.py
+++ dialogs/common/start.py
@@ -1,16 +1,17 @@
 from aiogram.enums import ParseMode
 from aiogram.fsm.state import StatesGroup, State
 from aiogram_dialog import Dialog, Window, DialogManager
-from aiogram_dialog.widgets.kbd import Button, Start, Column
+from aiogram_dialog.widgets.kbd import Button, Start, Column, Next, Url, Back
 from aiogram_dialog.widgets.text import Const, Format
 
 from dialogs.user.account import DAccount
 from dialogs.user.calendar import DCalendar
-from lexicon.lexicon import DC_START
+from lexicon.lexicon import DC_START, D_BUTTONS
 
 
 class DCommon(StatesGroup):
     start = State()
+    support = State()
 
 async def get_start_data(dialog_manager: DialogManager, **kwargs):
     # Получаем объект пользователя из события Telegram
@@ -33,10 +34,19 @@ dialog = Dialog(
                 id='calendar',
                 state=DCalendar.city
             ),
-            Button(Const(DC_START['buttons']['support']), id='support')
+            Next(Const(DC_START['buttons']['support']), id='support')
         ),
         parse_mode=ParseMode.HTML,
         getter=get_start_data,
         state=DCommon.start
+    ),
+    Window(
+        Const(DC_START['support']),
+        Url(
+            Const(DC_START['buttons']['support_msg']),
+            Const(DC_START['buttons']['support_url']),
+        ),
+        Back(Const(D_BUTTONS['back'])),
+        state=DCommon.support
     )
 )
